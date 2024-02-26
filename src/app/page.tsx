8caeb36873726e991326dfa24efcaf7e339aee1c
Anh Dang <anhhtdang@gmail.com>
2024-02-26 14:15:56 -0500

    Updated index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <body>
       <Navbar />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-bold">
-            HELLO WORLD
-        </h1>
-      </div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1 className="text-4xl font-bold">
+              HELLO WORLD
+          </h1>
+        </div>
+      </main>
+    </body>
   );
 }
