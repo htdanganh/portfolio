94041921891920addb869b575e32036586500e2a
Anh Dang <anhhtdang@gmail.com>
2024-03-02 14:51:42 -0500

    Added images to gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,25 +3,29 @@
 import Navbar from "@/components/ui/navbar";
 
 
-export default function Home() {
+export default function Gallery() {
   return (
     <body>
       <Navbar />
         <main className="flex min-h-screen flex-col items-center p-24">
           <div className="flex-container">
-            <h1 className="text-6xl font-bold">
-                Hello, I'm Anh
-            </h1>
             <br />
-            <p className="text-lg"> Lover of learning and creating all sorts of things. Aspiring software engineer. </p>
-            <p className="text-lg"> Check out my photos and projects using the links above. </p>
+            <p className="text-lg"> All photos taken with a Canon 7D with a 24-70mm lens. </p>
             <br />
-            <p className="text-lg"> Check out my photos using the links above. </p>
+            <img src="/images/soul-atl.jpeg" alt="Made with Soul in Atlanta sign" />
+            <br />
+            <img src="/images/just-be-happy.jpeg" alt="Just Be Happy sign" />
+            <br />
+            <img src="/images/coda.jpeg" alt="Coda spiral staircase" />
+            <br />
+            <img src="/images/nyclunch.jpeg" alt="NYC lunch in Brooklyn" />
+            <br />
+            <img src="/images/calibeachhouse.jpeg" alt="The side of a beachhouse in California" />
+            <br />
+            <img src="/images/biloxi.jpeg" alt="Biloxi" />
+            <br />
+            <img src="/images/water.jpeg" alt="Waves" />
           </div>
-          <br />
-          <img src="/images/throwback.jpeg" height="500px" width="500px"/>
-          <br />
-          <p className="text-med"> I love computer science!! </p>
         </main>
     </body>
   );
